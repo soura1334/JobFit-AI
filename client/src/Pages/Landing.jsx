9f5297a20ffb53d934d73365917f712b444ad79c
soura1334 <151785439+soura1334@users.noreply.github.com>
2025-08-04 20:47:24 +0530

    Revert "Manab"

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
-import Footer from '../Components/Footer'
-import AboutIt from '../Components/LandingPageParts/AboutIt'
 
 const Landing = () => {
   return (
       <div className=''>
         <Navbar/>
         <Hero/>
-        <AboutIt/>
-        <Footer/>
     </div>
   )
 }
